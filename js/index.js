5fc01fec57e7175b694b56bcf03c2202545622db
Greg Plumbly <greg.plumbly@gmail.com>
2023-09-21 09:55:13 +0100

    add employee cards in grid.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
-import employees from "./employees.js"
+import employees from "./employees.js";
+
+const selectElement = document.getElementById("team-filter");
+const mainElement = document.getElementById("main");
 
 // loop through employees array and get a list of unique teams
-const uniqueTeams = []
+const uniqueTeams = [];
 for (const employee of employees) {
   if (!uniqueTeams.includes(employee.team)) {
-    uniqueTeams.push(employee.team)
+    uniqueTeams.push(employee.team);
   }
 }
 
-console.log(uniqueTeams)
-
-const selectElement = document.getElementById("team-filter")
-
 for (const team of uniqueTeams) {
-const firstLetter = team.charAt(0)
-const firstLetterCap = firstLetter.toUpperCase()
-const remainingLetters = team.slice(1)
-const capitalizedTeam = firstLetterCap + remainingLetters
+  const firstLetter = team.charAt(0);
+  const firstLetterCap = firstLetter.toUpperCase();
+  const remainingLetters = team.slice(1);
+  const capitalizedTeam = firstLetterCap + remainingLetters;
+  selectElement.innerHTML += `<option value="${team}">${capitalizedTeam}</option>`;
+}
 
-    selectElement.innerHTML += `<option value="${team}">${capitalizedTeam}</option>`
+for (const emp of employees) {
+  mainElement.innerHTML += `  <div class="employee-card">
+                <img src=./images/photos/${emp.image} alt="Jeremy Robson" class="profile-photo">
+                <p>${emp.name}</p>
+                <p>${emp.title}</p>
+                <p>${emp.bio}</p>
+                </p>
+                <img src="./images/linkedin.png" alt="LinkedIn" class="socials">
+
+            </div>
+            `;
 }
